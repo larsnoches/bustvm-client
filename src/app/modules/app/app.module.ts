b64945ec614667949f3464ee1e258d7541fa9ebc
larsnoches <cyr.selyanin@gmail.com>
2022-03-08 16:00:25 +0500

    not-found component

--- src/app/modules/app/app.module.ts
+++ src/app/modules/app/app.module.ts
@@ -1,12 +1,13 @@
-import { AppComponent } from './components/app.component';
+import { AppComponent } from './components/app/app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { BrowserModule } from '@angular/platform-browser';
 import { BusTripsModule } from '@modules/bustrips/bustrips.module';
 import { NgModule } from '@angular/core';
+import { NotFoundPageComponent } from './components/not-found-page/not-found-page.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, NotFoundPageComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
