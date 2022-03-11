800cc0c6b15607d9efdaad13f45bf2baee70e82a
larsnoches <cyr.selyanin@gmail.com>
2022-03-11 21:06:25 +0500

    RedirectComponent sorted in import part

--- src/app/modules/core/core.module.ts
+++ src/app/modules/core/core.module.ts
@@ -2,8 +2,8 @@ import * as coreComponents from './components';
 import { AuthService } from './services/auth/auth.service';
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
-import { RouterModule } from '@angular/router';
 import { RedirectComponent } from './components/redirect/redirect.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [...coreComponents.components, RedirectComponent],
