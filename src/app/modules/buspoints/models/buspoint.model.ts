b7efdbf9519a851ad7dfb620849194d61c66bfe9
larsnoches <cyr.selyanin@gmail.com>
2022-03-26 00:50:03 +0500

    buspoints, fetch method for store service

--- src/app/modules/buspoints/models/buspoint.model.ts
+++ src/app/modules/buspoints/models/buspoint.model.ts
@@ -1,11 +1,57 @@
 import { BusPointType } from '@modules/buspoint-types/models/buspoint-type.model';
 import { BusTrip } from '@modules/bustrips/models/bustrip/bustrip.model';
+import { PageData } from '@helpers/page-data';
 
 export interface BusPoint {
   id: number;
   name: string;
   address: string;
-  busPointType: BusPointType;
-  departureBusTrips: Array<BusTrip>;
-  arrivalBusTrips: Array<BusTrip>;
+  busPointType?: BusPointType;
+  href: string;
+  departureBusTrips?: Array<BusTrip>;
+  arrivalBusTrips?: Array<BusTrip>;
+}
+
+export interface BusPointRequestDto {
+  name: string;
+  address: string;
+  busPointType: string;
+}
+
+export interface BusPointResponseDto {
+  id: number;
+  name: string;
+  address: string;
+  _links?: {
+    self: {
+      href: string;
+    };
+    busPoint?: {
+      href: string;
+    };
+    departureBusTrips?: {
+      href: string;
+    };
+    arrivalBusTrips?: {
+      href: string;
+    };
+    busPointType?: {
+      href: string;
+    };
+  };
+}
+
+export interface BusPointsResponseDto {
+  _embedded: {
+    busPoints: Array<BusPointResponseDto>;
+  };
+  _links?: {
+    self: {
+      href: string;
+    };
+    profile: {
+      href: string;
+    };
+  };
+  page?: PageData;
 }
--- src/app/modules/buspoints/services/buspoints/buspoints-store.service.ts
+++ src/app/modules/buspoints/services/buspoints/buspoints-store.service.ts
@@ -1,8 +1,84 @@
+import {
+  BusPoint,
+  BusPointsResponseDto,
+} from '@modules/buspoints/models/buspoint.model';
+import { PageData, initialPageData } from '@helpers/page-data';
+import { catchError, combineLatestWith, map, retry, tap } from 'rxjs';
+import { BehaviorSubjectItem } from '@helpers/behavior-subject-item';
+import { BusPointTypeStoreService } from '@modules/buspoint-types/services/buspoint-type-store/buspoint-type-store.service';
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { ThrowableService } from '@helpers/throwable-http-service';
+import { config } from '@helpers/config';
 
 @Injectable({
   providedIn: 'root',
 })
-export class BuspointsStoreService {
-  constructor() {}
+export class BusPointsStoreService extends ThrowableService {
+  loading = new BehaviorSubjectItem(false);
+  busPointData = new BehaviorSubjectItem<Array<BusPoint>>([]);
+  pageData = new BehaviorSubjectItem<PageData>(initialPageData());
+  public apiUrl = `${config.apiPath}/busPoints`;
+
+  constructor(
+    private bptStoreService: BusPointTypeStoreService,
+    private http: HttpClient,
+  ) {
+    super();
+    this.fetch();
+  }
+
+  fetch(): void {
+    this.http
+      .get<BusPointsResponseDto>(this.apiUrl)
+      .pipe(
+        tap(() => (this.loading.value = true)),
+        map((val): [Array<BusPoint>, PageData] | null => {
+          if (val == null) return null;
+          const { _embedded, page } = val;
+          if (_embedded == null) return [[], page];
+          const bps = _embedded?.busPoints?.map(dto => ({
+            id: dto.id,
+            name: dto.name,
+            address: dto.address,
+            href: dto._links?.self.href,
+          }));
+          return [bps, page];
+        }),
+        combineLatestWith(this.bptStoreService.busPointTypeData.value$),
+        map(([dto, busPointTypes]): [Array<BusPoint>, PageData] | null => {
+          if (dto == null) return null;
+          const [busPoints, page] = dto;
+          if (busPoints == null) return [[], page];
+          const bps = busPoints.map(val => {
+            const bpt = busPointTypes.find(
+              b => b.href === val.busPointType.href,
+            );
+            if (bpt == null) return val;
+            val.busPointType = { ...bpt };
+            return val;
+          });
+          return [bps, page];
+        }),
+        retry(3),
+        catchError(er => {
+          this.loading.value = false;
+          return this.handleError(er);
+        }),
+        tap(() => (this.loading.value = false)),
+      )
+      .subscribe(data => this.setBusPointData(data));
+  }
+
+  setBusPointData(value: [Array<BusPoint>, PageData]): void {
+    const [bps, page] = value;
+    if (bps != null) {
+      this.busPointData.value = bps;
+      this.busPointData.value.sort((a, b) => a.id - b.id);
+    }
+
+    if (page) {
+      this.pageData.value = page;
+    }
+  }
 }
